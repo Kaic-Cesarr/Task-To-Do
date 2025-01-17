302c51866389e05de4516a41fcf777b8f99a6848
Kaic Cesar <kaiccesar.dev@gmail.com>
2025-01-17 19:37:18 -0300

    Adding tasks to the HTML

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-tasksElement = document.querySelector('.tasks ul')
+tasksElement = document.querySelector("#tasks ul")
 inputElement = document.querySelector('.input-tasks');
 
+let tasks = [];
 
 function registerTasks() {
-    console.log(inputElement.value);
+    if (inputElement.value === ""){
+        alert("Digite algum tarefa");
+        return false;
+    } else {
+
+        let newTask = inputElement.value;
+
+        tasks.push(newTask);
+        inputElement.value = "";
+
+        renderTasks();
+    }
+}
+
+function renderTasks() {
+    // console.log(tasks);
+    tasksElement.innerHTML = "";
+
+    tasks.map((toDo) => {
+        let liElement = document.createElement("li");
+        let taskText = document.createTextNode(toDo);
+
+        tasksElement.appendChild(liElement);
+        liElement.appendChild(taskText);
+
+    });
 }
 
 
 function registerButton() {
     registerTasks();
+    
 }
 
