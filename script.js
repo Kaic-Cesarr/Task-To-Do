5c3c2eaef738467897b0bb7b4816bcdd980e8936
Kaic Cesar <kaiccesar.dev@gmail.com>
2025-01-17 12:27:01 -0300

    Starting the logic

---
+++ script.js
@@ -0,0 +1,13 @@
+tasksElement = document.querySelector('.tasks ul')
+inputElement = document.querySelector('.input-tasks');
+
+
+function registerTasks() {
+    console.log(inputElement.value);
+}
+
+
+function registerButton() {
+    registerTasks();
+}
+
